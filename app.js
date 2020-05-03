f7758460f0152539d3837e9eaf68709c9c086716
Saiprakash Holi <saiprakash.holi@in.abb.com>
2020-05-03 23:28:20 +0530

    added add and get user api's

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
 // app.listen(3600, () => {
 //   console.log("Servver is running in 3600 port")
 // })
@@ -26,13 +38,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,61 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(['respond with a resource', 'sass']);
+// router.get('/', function (req, res, next) {
+//   res.send(['respond with a resource', 'sass']);
+// });
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/payaniga");
+
+
+var users = new mongoose.Schema({
+  firstName: String,
+  lastName: String
 });
+
+var User = mongoose.model("users", users);
+
+
+
+
+router.post('/adduser', function (req, res, next) {
+  console.log("adduser: called ========================================== ")
+  console.log("myData : ", req.body)
+  var myData = new User(req.body);
+
+  myData.save()
+    .then(item => {
+      console.log("item : ", item)
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      console.log("====================================================", err)
+      res.status(400).send("unable to save to database");
+    });
+
+});
+
+
+
+router.get('/getusers', function (req, res, next) {
+  console.log("adduser: called ========================================== ")
+
+
+
+
+  User.find({}, (err, users) => {
+    // var userMap = {};
+
+    // users.forEach(function (user) {
+    //   userMap[user._id] = user;
+    // });
+
+    res.send(users);
+  });
+
+});
+
 
 module.exports = router;
